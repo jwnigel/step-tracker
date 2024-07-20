9995440369b3d2bce494cd485ff4b9ba15a8eac6
Nigel Wright <88900013+jwnigel@users.noreply.github.com>
2024-07-20 07:26:22 -0400

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Nigel Wright on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
