df52f8d172f56e9a9d089f189566076175543a48
Nigel Wright <88900013+jwnigel@users.noreply.github.com>
2024-08-08 19:38:29 -0400

    pie chart interactivity

--- Step Tracker/Charts/StepPieChart.swift
+++ Step Tracker/Charts/StepPieChart.swift
@@ -11,7 +11,18 @@ import Charts
 
 struct StepPieChart: View {
     
+    @State private var rawSelectedChartValue: Double? = 0
+    
     var chartData: [WeekdayChartData]
+    
+    var tappedWeekday: WeekdayChartData? {
+        guard let rawSelectedChartValue else { return nil }
+        var total = 0.0
+        return chartData.first {
+            total += $0.value
+            return rawSelectedChartValue <= total
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -30,13 +41,35 @@ struct StepPieChart: View {
                     SectorMark(
                         angle: .value("Average Steps", weekday.value),
                         innerRadius: .ratio(0.618),
+                        outerRadius: tappedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 140 : 110,
                         angularInset: 2
                     )
-                    .foregroundStyle(.pink)
+                    .foregroundStyle(.pink.gradient)
                     .cornerRadius(6)
+                    .opacity(tappedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 1.0 : 0.3)
                 }
             }
+            .chartAngleSelection(value: $rawSelectedChartValue.animation(.easeInOut))
             .frame(height: 240)
+            .chartBackground { proxy in
+                GeometryReader { geo in
+                    if let plotFrame = proxy.plotFrame {
+                        let frame = geo[plotFrame]
+                        if let tappedWeekday {
+                            VStack {
+                                Text(tappedWeekday.date.weekdayTitle)
+                                    .font(.title3.bold())
+                                    .animation(.interactiveSpring(duration: 0.1))
+                                
+                                Text(tappedWeekday.value, format: .number.precision(.fractionLength(0)))
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.secondary)
+                                    .contentTransition(.numericText())
+                            }
+                            .position(x: frame.midX, y: frame.midY)
+                        }
+                    }}
+            }
             
         }
         .padding()
